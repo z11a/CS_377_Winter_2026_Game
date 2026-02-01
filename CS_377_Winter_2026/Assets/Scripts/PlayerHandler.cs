cd4d1687c5d4909efe950fd0d49e52680587b9af
z11a <aidanzea865@gmail.com>
2026-01-31 19:08:08 -0600

    movement works and mouse model added, animation needs fixing

--- CS_377_Winter_2026/Assets/Scripts/PlayerHandler.cs
+++ CS_377_Winter_2026/Assets/Scripts/PlayerHandler.cs
@@ -7,16 +7,28 @@ public class PlayerHandler : MonoBehaviour
 {
     private PlayerInput playerInput;
     private Vector2 moveAmount;
-    public float playerSpeed = 10.0f;
     private CharacterController controller;
+    private Animator animator;
+
+    public float playerSpeed = 10.0f;
     public float gravity = -2.0f;
+    public static playerState _playerState;
+
+    public enum playerState 
+    {
+        Idle,
+        Running,
+        Aiming
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
-
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
+        _playerState = playerState.Idle;
     }   
 
     // Update is called once per frame
@@ -25,9 +37,37 @@ public class PlayerHandler : MonoBehaviour
     {
         if (GameStateManager._gameState == GameStateManager.GameState.inGame)
         {
-            Debug.Log("Waiting for movement input.");
-            
-            controller.Move(new Vector3(moveAmount.x, 0, moveAmount.y) * playerSpeed * Time.deltaTime);
+            MovementHandler();
+            AnimationHandler();
+        }
+    }
+
+    private void MovementHandler()
+    {
+        Debug.Log("Waiting for movement input.");
+        Vector3 movement = new Vector3(moveAmount.x, 0, moveAmount.y);
+
+        if (movement != Vector3.zero)
+        {
+            controller.Move(movement * playerSpeed * Time.deltaTime);
+            transform.forward = movement;
+            _playerState = playerState.Running;
+        }
+        else
+        {
+            _playerState = playerState.Idle;
+        }
+    }
+
+    private void AnimationHandler()
+    {
+        switch (_playerState)
+        {
+            case playerState.Idle:
+                break;
+            case playerState.Running:
+                animator.SetBool("Running", true);
+                break;
         }
     }
 
